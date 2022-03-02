2612b5c66f24ede94cf6bc34727ca4e4b80da290
dnjswndus95 <dnjswndus95@naver.com>
2022-03-02 14:04:28 +0900

    PasswordEncoder Done.

--- src/main/java/com/market/main/MainApplication.java
+++ src/main/java/com/market/main/MainApplication.java
@@ -4,6 +4,9 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.persistence.EntityManager;
 
@@ -16,4 +19,9 @@ public class MainApplication {
 
 	@Bean
 	JPAQueryFactory jpaQueryFactory(EntityManager em) {return new JPAQueryFactory(em);}
+
+	@Bean
+	public PasswordEncoder passwordEncoder(){
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
 }
--- src/main/java/com/market/main/service/MemberService.java
+++ src/main/java/com/market/main/service/MemberService.java
@@ -5,12 +5,14 @@ import com.market.main.entity.member.MemberForm;
 import com.market.main.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,6 +27,7 @@ public class MemberService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
+
 
 
     @Override
